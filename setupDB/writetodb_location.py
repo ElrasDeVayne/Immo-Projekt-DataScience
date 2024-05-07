17735c53b319db974c57666274cf2aa84b453459
aberweger <145767058+aberweger@users.noreply.github.com>
2024-05-07 17:49:02 +0200

    Update WriteToDB mit Location V6

--- setupDB/writetodb_location.py
+++ setupDB/writetodb_location.py
@@ -6,12 +6,12 @@ conn_str = 'DRIVER={SQL Server};SERVER=ANNINAYOGA\SQLEXPRESS;DATABASE=Immo;Trust
 
 
 
-data = pd.read_csv (r'C:\Users\AnninaBerweger\datascience-immo\Immo-Projekt-DataScience\Location_v2.csv',sep=',')
+data = pd.read_csv (r'C:\Users\AnninaBerweger\datascience-immo\Immo-Projekt-DataScience\TablesDB\Location_v3_enriched_v6.csv',sep=',')
 df = pd.DataFrame(data)
 print(df.shape)
-df = df[df['ZIP']>0]
+df = df[df['zip']>0]
 print(df.shape)
-df['ZIP'] = df['ZIP'].astype("int64")
+df['zip'] = df['zip'].astype("int64")
 df['longitude'] = df['longitude'].astype("string")
 df['latitude'] = df['latitude'].astype("string")
 # Connect to SQL Server
@@ -25,13 +25,23 @@ df = df.replace({np.nan:None})
 for row in df.itertuples():
     try:
         cursor.execute('''
-                    INSERT INTO Location ([LocationId],[Street],[ZIP],[longitude],[latitude])
-                    VALUES (?,?,?,?,?)''',
+                    INSERT INTO Location ([LocationId],[street],[zip],[longitude],[latitude],[bfs_number],[municipality],[incometax_canton],[incometax_municipality],[wealthtax_canton],[wealthtax_municipality],[population_density],[public_transport_count],[supermarket_count],[foodandbeverage_count])
+                    VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''',
                     row.LocationId,
-                    row.Street,
-                    row.ZIP,
+                    row.street,
+                    row.zip,
                     row.longitude,
-                    row.latitude
+                    row.latitude,
+                    row.bfs_number,
+                    row.municipality,
+                    row.incometax_canton,
+                    row.incometax_municipality,
+                    row.wealthtax_canton,
+                    row.wealthtax_municipality,
+                    row.population_density,
+                    row.public_transport_count,
+                    row.supermarket_count,
+                    row.foodandbeverage_count
                     )
         conn.commit()
     except Exception as e:
